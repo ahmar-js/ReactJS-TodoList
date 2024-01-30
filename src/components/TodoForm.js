df56361c9bcff3f8a93e7eac9253706f01b0228a
Ahmar Aamir <ahmaraamir33@gmail.com>
2024-01-30 17:32:16 +0500

    Feature: Edit Task

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -13,7 +13,7 @@ export const TodoForm = ({addTodo}) => {
   return (
     <form className='TodoForm' onSubmit={handleSubmit}>
         <input type='text' className='todo-input' value={value} placeholder='What is the task today?' 
-        onChange={(e) => setValue(e.target.value)}/>
+        onChange={(e) => setValue(e.target.value)} required/>
         <button type='submit' className='todo-btn'>Add Task</button>
     </form>
   )
--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react'
 import { TodoForm } from './TodoForm'
 import {v4 as uuidv4} from 'uuid';
 import { Todo } from './Todo';
+import { EditTodoForm } from './EditTodoForm';
 uuidv4();
 
 export const TodoWrapper = () => {
@@ -18,30 +19,48 @@ export const TodoWrapper = () => {
         localStorage.setItem('todos', JSON.stringify(newTodos));
         console.log(newTodos)
       };
-    const toggleComplete = id => {
+    const toggleComplete = (id) => {
         setTodos(todos.map(todo => todo.id === id ? {...todo, completed: !todo.completed} : todo));
     }
-    const deleteTodo = id => {
+    const deleteTodo = (id) => {
         // filtering each todo that is not euqal to todo id
         // Basically if the todo.id is not equal to the id we are passing then we will return the todo value
         setTodos(todos.filter(todo => todo.id !== id))
         // Update local storage after deleting the todo
         localStorage.setItem('todos', JSON.stringify(todos.filter(todo => todo.id !== id)));
     }
-  return (
-    <div className='TodoWrapper'>
-        <h1>To Do Things!</h1>
+    const editTodo = (id) => {
+        setTodos(todos.map((todo) => todo.id === id ? {...todo, isEditing: !todo.isEditing} : todo))
+        
+    }
+    const editTask = (task, id) => {
+      // Update todos with the edited task and toggle isEditing
+      const updatedTodos = todos.map((todo) => (todo.id === id ? { ...todo, task, isEditing: !todo.isEditing } : todo));
+      setTodos(updatedTodos);
+    
+      // Update local storage after editing the task
+      localStorage.setItem('todos', JSON.stringify(updatedTodos));
+    };
+    
+  
+    return (
+      <div className="TodoWrapper">
+        <h1>To Do Things !</h1>
         <TodoForm addTodo={addTodo} />
-        {/* Generate Todo for each value in the state */}
-        {todos.map((todo, index) => (
-    <Todo
-        task={todo}
-        key={index}
-        toggleComplete={toggleComplete}
-        deleteTodo={deleteTodo}
-    />
-))}
-
-    </div>
-  )
-}
+        {/* display todos */}
+        {todos.map((todo) =>
+          todo.isEditing ? (
+            <EditTodoForm editTodo={editTask} task={todo} />
+          ) : (
+            <Todo
+              key={todo.id}
+              task={todo}
+              deleteTodo={deleteTodo}
+              editTodo={editTodo}
+              toggleComplete={toggleComplete}
+            />
+          )
+        )}
+      </div>
+    );
+  };
